49c56513ea14383bb256022e16cd07b8c6ec6204
Faiza Ahmed <shamim.faiza@gmail.com>
2023-11-01 18:32:43 -0400

    second commit

--- src/main/java/com/myfirstspringproject/demo/myfirstapp/rest/FunRestController.java
+++ src/main/java/com/myfirstspringproject/demo/myfirstapp/rest/FunRestController.java
@@ -1,5 +1,6 @@
 package com.myfirstspringproject.demo.myfirstapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +10,23 @@ public class FunRestController {
 //	expose "/" that return Hello World
 	
 	@GetMapping("/")
-	
 public String helloworld() {
-		return "Hello World";
+		return "Hello World from faiza";
 	}
+	
+	@GetMapping("/workout")
+	public String workout() {
+			return "Running 5k today";
+		}
+	
+	@Value("${coach.name}")
+			private String coachName;
+	
+	@Value("${team.name}")
+			private String teamName;
+	
+	@GetMapping("/teaminfo")
+	public String TeamInfo() {
+			return "Coach name is: " +coachName+ " and Team name is:   "+teamName  ;
+		}
 }
